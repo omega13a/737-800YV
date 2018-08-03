d4ba0e4b2a51ea9db5aa724ac8da2b15f4c2e9c0
omega13a <omega13a@yahoo.com>
2018-08-03 16:55:04 -0700

    Don't know how all that got in there...

--- Nasal/The magical health benefits of getting cold_files/satellite-57ea708964746d28ea001468.js
+++
@@ -1,9 +0,0 @@
-_satellite.pushAsyncScript(function(event, target, $variables){
-  (function(d) {
-    if (/^\/travel\/ski/.test(window.location.pathname)) {
-        var txtNode = document.createTextNode('<!-- TradeDoubler site verification 2884512 -->');
-        d.getElementsByTagName('head')[0].appendChild(txtNode);
-    }
-})(document);
-
-});
--- Nasal/The magical health benefits of getting cold_files/satellite-58dbb66c64746d396f00bd02.js
+++
@@ -1,6 +0,0 @@
-_satellite.pushAsyncScript(function(event, target, $variables){
-  if (typeof s != 'undefined') {
-	window.optimizely2 = window.optimizely2 || [];
-	window.optimizely2.push(['activateSiteCatalyst', {"sVariable": s}]);
-}
-});
